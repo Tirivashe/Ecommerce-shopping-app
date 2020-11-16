a1da38eadead42d0c50528d0180f6fa70e0df8e0
Tiri <shaymusts@gmail.com>
2020-11-15 21:07:08 -0600

    Added routing components

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Header from './components/Header/Header'
 import HomePage from './pages/HomePage/HomePage'
 import './App.css'
 import { CssBaseline } from '@material-ui/core'
+import { Switch, Route } from 'react-router-dom'
+import Registration from './pages/Registration/Registration'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header/>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/registration" component={Registration}/>
+      </Switch>
       <CssBaseline />
-    </div>
+    </>
   );
 }
 
