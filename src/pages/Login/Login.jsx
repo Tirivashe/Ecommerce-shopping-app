9c55662e547518c7eee25aa9cf5829c1405bccc6
Tiri <shaymusts@gmail.com>
2020-11-18 14:04:08 -0600

    Added login page and functionality

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { Avatar, Button, CssBaseline, Typography, Container } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Avatar, Button, CssBaseline, Typography, Container, TextField } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { useStyles } from './styles'
-import { signInWithGoogle } from '../../firebase/utils'
+import { auth ,signInWithGoogle } from '../../firebase/utils'
 
 export default function SignIn() {
-  const classes = useStyles();
+  const initialState = {
+    email:"",
+    password: ""
+  }
+  const [fields, setFields] = useState(initialState)
 
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const {email, password } = fields
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      setFields(initialState)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleChange = e => {
+    const { name, value } = e.target
+    setFields(prevState =>{
+      return {...prevState, [name]: value}
+    })
+  }
+
+  const classes = useStyles();
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -17,15 +41,53 @@ export default function SignIn() {
         <Typography component="h1" variant="h5">
           Log in
         </Typography>
-        <Button
-            onClick={signInWithGoogle}
+        <form onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={fields.email}
+            onChange={handleChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={fields.password}
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
           >
+            Sign In
+          </Button>
+          <Typography align="center">OR</Typography>
+          <Button
+            onClick={signInWithGoogle}
+            fullWidth
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+            >
             Sign In With Google
           </Button>
+        </form>
       </div>
     </Container>
   );
