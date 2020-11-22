5c0abca21337686cb8946790635ee983dc64fa7e
Tiri <shaymusts@gmail.com>
2020-11-21 23:31:38 -0600

    Added admin toolbar component, redux dispatches, admin page route and admin check

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,53 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Header from './components/Header/Header'
+import AdminToolbar from './components/AdminToolbar/AdminToolbar'
 import HomePage from './pages/HomePage/HomePage'
+import Registration from './pages/Registration/Registration'
+import Login from './pages/Login/Login'
+import Recovery from './pages/Recovery/Recovery'
+import Admin from "./pages/Admin/Admin"
 import './App.css'
 import { CssBaseline } from '@material-ui/core'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import Registration from './pages/Registration/Registration'
-import Login from './pages/Login/Login'
-import { auth, handleUserProfile } from './firebase/utils'
-import Recovery from './pages/Recovery/Recovery'
+import { auth, handleUserProfile, checkUserAdmin} from './firebase/utils'
+import { useSelector, useDispatch } from 'react-redux'
+import { setCurrentUser } from './redux/slices/user'
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null)
-  console.log(currentUser)
+  const currentUser = useSelector(state => state.user)
+  const dispatch = useDispatch()
+
+  const isAdmin = checkUserAdmin(currentUser)
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged( async user => {
       if(user){
-      await handleUserProfile(user)
-      setCurrentUser(user)
+        const userRef = await handleUserProfile(user)
+        userRef.onSnapshot(snapshot => {
+          const userInfo = {
+            id: snapshot.id,
+            ...snapshot.data()
+          }
+          dispatch(setCurrentUser(userInfo))
+        })
       }else{
-        setCurrentUser(null)
+        dispatch(setCurrentUser(user))
       }
     })
     return () => unsubscribe()
-  },[])
+  },[dispatch])
 
   return (
     <>
+      <AdminToolbar />
       <Header currentUser={currentUser}/>
       <Switch>
-        <Route exact path="/" render={()=> <HomePage currentUser={currentUser}/>} />
+        <Route exact path="/" render={()=> currentUser ? <HomePage currentUser={currentUser}/> : <Redirect to="/login" /> }/>
         <Route path="/registration" render={()=> currentUser ? <Redirect to="/"/> : <Registration currentUser={currentUser} /> }/>
         <Route path="/login" render={()=> currentUser ? <Redirect to="/"/> : <Login /> }/>
         <Route path="/recovery" render={()=> <Recovery />}/>
+        <Route exact path="/admin" render={()=> isAdmin ? <Admin /> : <Redirect to="/" />} />
+
       </Switch>
       <CssBaseline />
     </>
