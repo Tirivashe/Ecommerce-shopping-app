c0516058a83a798f8dd0a899de737e78f6b511e3
Tiri <shaymusts@gmail.com>
2020-11-26 23:22:09 -0600

    Added admin toolbar styles

--- src/components/AdminToolbar/AdminToolbar.jsx
+++ src/components/AdminToolbar/AdminToolbar.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import { checkUserAdmin } from '../../firebase/utils'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { useStyles } from './styles'
 
 
 export default function AdminToolbar() {
   const currentUser = useSelector(state => state.user)
   const isAdmin = checkUserAdmin(currentUser)
+  const classes = useStyles()
 
   if(!isAdmin){
     return null 
   } else{
     return (
-      <div>
-        <Link to="/admin">Go To Admin Page</Link>
+      <div className={classes.root}>
+        <Link to="/admin" className={classes.link}>Go To Admin Page</Link>
       </div>
     )
   }
